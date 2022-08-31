7e19f6f455ae9e865081025074e4ba7819e17c4f
Vincent <vincent@Macs-MacBook-Pro-2.local>
2022-08-31 09:30:27 +0100

    MLP-352: create a web app for pre evaluation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,8 @@
 <template>
     <div>
-       <div class="pos-f-t px-3">
-        <div class="collapse" id="navbarToggleExternalContent">
-            <div class="bg-white p-4">
-            <h5 class="text-white h4">Collapsed content</h5>
-            <span class="text-muted">Toggleable via the navbar brand.</span>
-            </div>
-        </div>
-        <nav class="navbar navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" 
-            aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <h5 class="justify-content-center">{{title}}</h5>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdownMenuLink"
-                    role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp" class="rounded-circle"
-                        height="22" alt="Avatar" loading="lazy" />
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <li>
-                        <a class="dropdown-item" href="#">My profile</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">Settings</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">Logout</a>
-                    </li>
-                    </ul>
-                </li>
-            </ul>
+       <div class="pos-f-t px-3 py-2">
+        <nav class="container-fluid-nav navbar navbar-light bg-light">
+            <h5 class="d-flex text-align-center">{{title}}</h5>
         </nav>
       </div>
     </div>   
